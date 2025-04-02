a4e40b3d62b8901244201fcef5713bfb0b3de5c8
Matheus Levy <srlevy008@gmail.com>
2025-04-02 11:41:07 -0300

    :,)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,14 +8,31 @@ var healtBar
 
 func _ready() -> void:
 	healtBar = $"../CanvasLayer/HealthBar"
-	healtBar.value = 100
+	
+	
+
 func _physics_process(delta: float) -> void:
 	moviment()
 	move_and_slide()
 	life()
-	
+
 func life():
 	healtBar.value = heatl
+	
+func _on_hurt_box_area_entered(area):
+	if area.name != "hitBox": return
+
+	heatl -= 30
+	
+	print_debug(heatl)
+	
+	var knockback_force = 1000
+	var knockback_direction = (global_position - area.global_position).normalized()
+	velocity = knockback_force * knockback_direction
+	move_and_slide()
+
+	if heatl <= 0:
+		get_tree().reload_current_scene()
 func moviment():
 	var input_direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
